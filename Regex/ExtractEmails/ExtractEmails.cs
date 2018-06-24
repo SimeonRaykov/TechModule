01fbfb0bb8a58a4e4efb360feadc41c002ee0e51
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-24 21:01:28 +0300

    Add files via upload

---
+++ Regex/ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string input = Console.ReadLine();
+            string regex =
+                @"(^|(?<=\s))([A-Za-z0-9]+)([-_.][A-Za-z0-9]+)?@([A-Za-z0-9]+\b[-]?[A-z0-9a-z]?)+.([A-Za-z0-9])+([.]?[A-Za-z0-9]*)\b";
+
+            MatchCollection emails= Regex.Matches(input, regex);
+
+            
+
+            foreach (Match email in emails)
+            {
+
+                Console.WriteLine(email.Value);
+
+            }
+
+
+        }
+    }
+}
---
+++ Regex/ExtractSentencesByKeyWord/ExtractSentencesByKeyWord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentencesByKeyWord
+{
+    class ExtractSentencesByKeyWord
+    {
+        static void Main(string[] args)
+        {
+
+            string word = Console.ReadLine();
+            string input = Console.ReadLine();
+            string[] sentences = input.Split(new char[] {'!', '.', '?'}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+
+
+            string pattern = $@"\b{word}\b";
+
+
+            foreach (var sentence in sentences)
+            {
+
+                if (Regex.IsMatch(sentence,pattern))
+                {
+                    Console.WriteLine(sentence.Trim());
+                }
+                
+            }
+
+
+
+        }
+    }
+}
