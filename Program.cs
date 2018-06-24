d463e7b28033a844e096eacee314146206c7f597
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-24 20:03:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String command = Console.ReadLine();
+            int count = 0;
+         
+            while (command!="Bake!")
+            {
+
+                Console.WriteLine($"Adding ingredient {command}.");
+                command=Console.ReadLine();
+                count++;
+                
+                if (command == "Bake!")
+                {
+                    Console.WriteLine($"Preparing cake with {count} ingredients.");
+                    return;
+                }
+               
+                    
+                    
+                
+            }
+
+
+        }
+    }
+}
