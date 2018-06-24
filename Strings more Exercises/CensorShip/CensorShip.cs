98107b4bc2bca8a86714cd3ceaa89a4853360bf3
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-24 23:27:58 +0300

    Add files via upload

---
+++ Strings more Exercises/CensorShip/CensorShip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CensorShip
+{
+    class CensorShip
+    {
+        static void Main(string[] args)
+        {
+
+            string bannedWord = Console.ReadLine();
+
+            string mex = new string('*', bannedWord.Length); 
+
+            string input = Console.ReadLine();
+            string pattern = $@"{bannedWord}";
+            string result = Regex.Replace(input, pattern, mex);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
