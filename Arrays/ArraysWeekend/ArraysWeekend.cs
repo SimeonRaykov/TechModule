a58b88b88ec9b66bab693e123d9a9957ce5f2d04
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-24 20:55:52 +0300

    Add files via upload

---
+++ Arrays/ArraysWeekend/ArraysWeekend.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArraysWeekend
+{
+    class ArraysWeekend
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                string[] daysOfWeek = new string[7];
+
+                daysOfWeek[0] = "Monday";
+                daysOfWeek[1] = "Tuesday";
+                daysOfWeek[2] = "Wednesday";
+                daysOfWeek[3] = "Thursday";
+                daysOfWeek[4] = "Friday";
+                daysOfWeek[5] = "Saturday";
+                daysOfWeek[6] = "Sunday";
+
+
+                Console.WriteLine($"{daysOfWeek[number - 1]}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            
+
+
+
+        }
+    }
+}
+
---
+++ Arrays/ReverseAnArrayOfIntegers/ReverseAnArrayOfIntegers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAnArrayOfIntegers
+{
+    class ReverseAnArrayOfIntegers
+    {
+        static void Main(string[] args)
+        {
+
+            int counterOfNumbersInArray = int.Parse(Console.ReadLine());
+            int[] arrayOfNumbers = new int[counterOfNumbersInArray];
+
+            for (int i = 0; i < counterOfNumbersInArray; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                arrayOfNumbers[i] = currentNumber;
+       
+            }
+                    int[] reversedArrayOfNumbers = arrayOfNumbers
+                .Reverse()
+                .ToArray();
+//Print This
+
+      //     for (int i = 0; i < reversedArrayOfNumbers.Length; i++)
+      //     {
+      //         Console.WriteLine(reversedArrayOfNumbers[i]);
+      //     }
+
+            //Or this 
+             
+            foreach (var item in reversedArrayOfNumbers)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+        }
+    }
+}
