a5bea2498e06401c89ab8aa0c7dd78b07a979061
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-24 20:52:40 +0300

    Add files via upload

---
+++ Methods and Debugging/CalculateTriangleArea/CalcTriangleAreaMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculateTriangleArea
+{
+    class CalcTriangleAreaMethods
+    {
+        static void Main(string[] args)
+        {
+
+            double height = Double.Parse(Console.ReadLine());
+            double width = Double.Parse(Console.ReadLine());
+            double areaSize = TriangleArea(height,width);
+            Console.WriteLine($"{areaSize}");
+
+        }
+        static double TriangleArea(double height, double width)
+        {
+            return height * width / 2;
+        }
+    }
+}
---
+++ Methods and Debugging/DrawAFilledSquare/DrawAFilledSquare.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DrawAFilledSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            TopAndBot(n);
+            Middle(n);
+            TopAndBot(n);
+
+        }
+
+        private static void TopAndBot(int n)
+        {
+
+            string topAndBot = new string('-', n * 2);
+            Console.WriteLine(topAndBot);
+        }
+        static void Middle(int n)
+        {
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write("-");
+                for (int j = 0; j < n - 1; j++)
+                {
+
+                    Console.Write(@"\/");
+                }
+                Console.WriteLine("-");
+            }
+
+
+        }
+    }
+}
+
