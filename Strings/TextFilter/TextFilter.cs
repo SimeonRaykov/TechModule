431f474db7a2b1b062823fc5dec0235774c891db
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-25 13:59:43 +0300

    Add files via upload

---
+++ Strings/TextFilter/TextFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace TextFilter
+{
+    class TextFilter
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] bannedWords = Console.ReadLine()
+                .Split(" ,".ToCharArray()
+                    , StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string input = Console.ReadLine();
+
+
+
+            foreach (var bannedWord in bannedWords)
+            {
+
+                input = input.Replace(bannedWord, new string('*', bannedWord.Length));
+
+
+
+            }
+
+            Console.WriteLine(input);
+        
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Strings/palindromeCheckerSorted/palindromeCheckerSorted.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace palindromeCheckerSorted
+{
+    class palindromeCheckerSorted
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(" ,.?!".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> words = new List<string>();
+            bool isPalindrome;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (getStatus(input[i]))
+                {
+                    words.Add(input[i]);
+                }
+
+            }
+
+            words.Sort();
+            words = words.Distinct().ToList();
+
+
+
+            Console.WriteLine(string.Join(", ", words));
+
+
+        }
+
+        public static bool getStatus(string myString)
+        {
+            string first = myString.Substring(0, myString.Length / 2);
+            char[] arr = myString.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+            return first.Equals(second);
+        }
+    }
+}
