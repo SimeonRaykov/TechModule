7afa9d7e792e2be957bf7bfe1a2cd67f59c1f76c
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-25 15:39:54 +0300

    Add files via upload

---
+++ Regex/regexReplace/RegexReplace.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace regexReplace5
+{
+    class RegexReplace
+    {
+        static void Main(string[] args)
+        {
+
+            var keys = Console.ReadLine();
+            var textInput = Console.ReadLine();
+
+            string pattern = @"(.+)([|\\\<]{1})(.+)([|\\\<]{1})(.+)";
+
+            Regex regex=new Regex(pattern);
+
+
+            var match = Regex.Match(keys, pattern);
+
+            var keyStart = match.Groups[1];
+            var keyEnd = match.Groups[5];
+
+            string wordPattern = $@"(?<={keyStart})(.+)(?={keyEnd})";
+
+            var finalMatch = Regex.Match(textInput, wordPattern);
+
+            var finalString = "";
+
+            var wordzPattern = $@"{keyStart}(?!{keyEnd})(.*?){keyEnd}";
+
+            MatchCollection matchedCollection = Regex.Matches(textInput, wordzPattern);
+
+            foreach (Match coll in matchedCollection)
+            {
+
+                finalString += coll.Groups[1].Value;
+
+
+            }
+
+            if (finalString != "")
+            {
+            Console.WriteLine(finalString);
+
+            }
+            else
+            {
+                Console.WriteLine("Empty result");
+            }
+        }
+    }
+}
