7f46bd3594fdbd06c4f90c7adf8824f5a5aee041
SimeonRaykov <40491620+SimeonRaykov@users.noreply.github.com>
2018-06-30 00:06:42 +0300

    Add files via upload

---
+++ Exams/Retake Exam 6 January/EnduranceRally/EnduranceRally.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnduranceRally
+{
+    class EnduranceRally
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] names = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            double[] paths = Console.ReadLine()
+                .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            List<int> checkPoints = Console.ReadLine().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            var startingFuel = 0;
+            double sum = 0;
+
+
+            foreach (var name in names)
+            {
+
+                startingFuel = name[0];
+                sum = startingFuel;
+
+                for (int i = 0; i < paths.Length; i++)
+                {
+                    if (checkPoints.Contains(i))
+                    {
+                        sum += paths[i];
+                    }
+                    else
+                    {
+                        sum -= paths[i];
+                    }
+
+                    if (sum <= 0)
+                    {
+                        Console.WriteLine($"{name} - reached {i}");
+                        break;
+                    }
+
+
+                }
+
+                if (sum > 0)
+                {
+                    Console.WriteLine($"{name} - fuel left {sum:F2}");
+                }
+
+                sum = 0;
+
+            }
+        }
+    }
+}
---
+++ Exams/Retake Exam 6 January/WinningTicket/WinningTicket.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+
+namespace WinningTicket
+{
+    class WinningTicket
+    {
+        static void Main(string[] args)
+        {
+
+            string[] tickets = Console.ReadLine().Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length == 20)
+                {
+                    var firstHalf = ticket.Take(10).ToArray();
+                    var secondHalf = ticket.Skip(10).Take(10).ToArray();
+
+                    var counterLeft = 0;
+                    var counterRight = 0;
+                    var bestSeqLeft = 0;
+                    var bestSeqRight = 0;
+                    var currentSymbol = "";
+                    var bestSymbolLeft = "";
+                    var bestSymbolRight = "";
+                    var matchSymbolLeft = firstHalf[0].ToString();
+                    var matchSymbolRight = secondHalf[0].ToString();
+
+
+                    for (int i = 0; i < firstHalf.Length; i++)
+                    {
+                        currentSymbol = firstHalf[i].ToString();
+
+
+                        if (currentSymbol == "$" || currentSymbol == "#"
+                                                 || currentSymbol == "@" || currentSymbol == "^")
+                        {
+                            counterLeft += 1;
+                            if (currentSymbol != matchSymbolLeft)
+                            {
+                                counterLeft = 1;
+                                matchSymbolLeft = currentSymbol;
+                            }
+
+                            if (counterLeft > bestSeqLeft)
+                            {
+                                bestSeqLeft = counterLeft;
+                                bestSymbolLeft = currentSymbol;
+                                matchSymbolLeft = currentSymbol;
+                            }
+
+
+                        }
+                        else
+                        {
+                            counterLeft = 1;
+                        }
+
+
+                    }
+
+
+                    for (int i = 0; i < secondHalf.Length; i++)
+                    {
+                        currentSymbol = secondHalf[i].ToString();
+
+
+                        if (currentSymbol == "$" || currentSymbol == "#"
+                                                 || currentSymbol == "@" || currentSymbol == "^")
+                        {
+                            counterRight += 1;
+
+                            if (currentSymbol != matchSymbolRight)
+                            {
+                                counterRight = 1;
+                                matchSymbolRight = currentSymbol;
+                            }
+
+
+                            if (counterRight > bestSeqRight)
+                            {
+                                bestSeqRight = counterRight;
+                                bestSymbolRight = currentSymbol;
+                                matchSymbolRight = currentSymbol;
+                            }
+
+
+                        }
+                        else
+                        {
+                            counterRight = 1;
+                        }
+
+
+                    }
+
+                    if (bestSeqRight == bestSeqLeft
+                        && bestSymbolLeft == bestSymbolRight
+                        && bestSeqRight >= 6)
+                    {
+
+
+                        if (bestSeqRight == 10)
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - 10{bestSymbolLeft} Jackpot!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - {bestSeqLeft}{bestSymbolRight}");
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - no match ");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"invalid ticket");
+                }
+
+            }
+
+        }
+    }
+}
